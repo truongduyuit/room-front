ad86c4879cddf5656fe431fffa43d27eebb179cf
truongduyuit <uit.truongduy@gmail.com>
2020-07-17 14:19:09 +0700

    Fix UI Home

--- src/components/web/Home/RateUser.js
+++ src/components/web/Home/RateUser.js
@@ -6,26 +6,27 @@ export default class RateUser extends Component {
         return (
             <div className="rate" id="rate">
                 <div className="info-rate">
-                    <h1>What Clients Says About Us</h1>
+                    <h1>Khách hàng nói gì </h1>
                     <br />
-                    <h3>
-                        Duis et metus et massa tempus lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas ultricies, orci molestie blandit interdum.
-
-ipsum ante pellentesque nisl, eget mollis turpis quam nec eros. ultricies, orci molestie blandit interdum.
-                    </h3>
+                    <h5>
+                        Sự hài lòng của khách hàng là động lực để chúng tôi cải thiện phần mềm và mở ra cơ hội cho nhiều khách hàng mới hơn trong tương lai. Do đó, chúng tôi rất coi trọng phản hồi của khách hàng và nỗ lực để tận dụng tốt nhất những phản hồi đó.
+                    </h5>
                 </div>
 
                 <div className= "rates">
                     <Carousel autoplay>
                         <div>
                             <div className="rate-slide">
-                                <h3>“Praesent scelerisque, odio eu fermentum malesuada, nisi arcu volutpat nisl, sit amet convallis nunc turp.”</h3>
+                                <h3>“Phần mềm giúp tôi quản lý người thuê, hợp đồng và hóa đơn hàng tháng rất dễ dàng.”</h3>
                                 <br />
                                 <hr />
                                 <br />
                                 <div className="rate-star">
                                     <Avatar size={100} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                                    <strong>Mr. Johnson</strong>
+                                    <div>
+                                        <b>Anh Nguyễn Văn Thành</b>
+                                        <div>Chủ trọ</div>
+                                    </div>
                                     <Rate disabled defaultValue={5} />
                                 </div>
                             </div>
@@ -33,28 +34,15 @@ ipsum ante pellentesque nisl, eget mollis turpis quam nec eros. ultricies, orci 
                         </div>
                         <div>
                             <div className="rate-slide">
-                                <h3>“Praesent scelerisque, odio eu fermentum malesuada, nisi arcu volutpat nisl, sit amet convallis nunc turp.”</h3>
-                                <br />
-                                <hr />
-                                <br />
-                                <div className="rate-star">
-                                    <Avatar size={100} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                                    <strong>Mr. Johnson</strong>
-                                    <Rate disabled defaultValue={5} />
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="rate-slide">
-                                <h3>“Praesent scelerisque, odio eu fermentum malesuada, nisi arcu volutpat nisl, sit amet convallis nunc turp.”</h3>
+                                <h3>“Kể từ khi sử dụng phần mềm, việc quản lý nhà trọ của tôi trở nên đơn giản hơn nhiều.”</h3>
                                 <br />
                                 <hr />
                                 <br />
                                 <div className="rate-star">
                                     <Avatar size={100} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
                                     <div>
-                                        <b>Mr. Johnson</b>
-                                        <div>U.S.A</div>
+                                        <b>Chị Lê Thị Hạnh</b>
+                                        <div>Chủ trọ</div>
                                     </div>
                                     <Rate disabled defaultValue={5} />
                                 </div>
@@ -62,13 +50,32 @@ ipsum ante pellentesque nisl, eget mollis turpis quam nec eros. ultricies, orci 
                         </div>
                         <div>
                             <div className="rate-slide">
-                                <h3>“Praesent scelerisque, odio eu fermentum malesuada, nisi arcu volutpat nisl, sit amet convallis nunc turp.”</h3>
+                                <h3>“Giao diện thân thiện và rất dễ sử dụng, nhiều tính năng hữu ích, phù hợp với mọi chủ trọ.”</h3>
                                 <br />
                                 <hr />
                                 <br />
                                 <div className="rate-star">
                                     <Avatar size={100} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                                    <strong>Mr. Johnson</strong>
+                                    <div>
+                                        <b>Anh Nguyễn Văn Thành</b>
+                                        <div>Chủ trọ</div>
+                                    </div>
+                                    <Rate disabled defaultValue={5} />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="rate-slide">
+                                <h3>“Phần mềm giúp mình quản lý khách thuê, hợp đồng và hóa đơn hàng tháng rất dễ dàng.”</h3>
+                                <br />
+                                <hr />
+                                <br />
+                                <div className="rate-star">
+                                    <Avatar size={100} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                    <div>
+                                        <b>Bác Phạm Thị Cúc</b>
+                                        <div>Chủ trọ</div>
+                                    </div>
                                     <Rate disabled defaultValue={5} />
                                 </div>
                             </div>
--- src/components/web/Home/Team.js
+++ src/components/web/Home/Team.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Card} from 'antd';
 import {Row, Col} from 'reactstrap';
 
+// import "../../../assets/img/123.jpg"
+
 const {Meta} = Card;
 
 export default class Team extends Component {
@@ -9,15 +11,17 @@ export default class Team extends Component {
         return (
             <div className= "our-team" id="our-team">
                 <div className="info-team">
-                    <h1>Meet the Team</h1>
-                    <h3>Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</h3>
+                    <h1>Đội Ngủ</h1>
+                    <h5>Chúng tôi luôn cố gắng tạo ra môi trường làm việc chuyên nghiệp, sáng tạo và kỷ luật cao. Đội ngũ kỹ sư trẻ giàu nhiệt huyết và các nhân viên tư vấn khách hàng luôn sẵn sàng hỗ trợ bạn suốt 24/7, phần mềm quản lý nhà trọ luôn được phát triển hàng ngày.
+                    </h5>
                 </div>
 
                 <div className="team">
                     <Card
                         hoverable
                         style={{width: 300}}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                        cover={<img alt="example" src="https://i.imgur.com/xAJQihv.jpg" />}
+                        
                     >
                         <Meta title="Chung Nguyền Trường Duy" description="Software Engineer" />
                         <br />
@@ -39,7 +43,7 @@ export default class Team extends Component {
                     <Card
                         hoverable
                         style={{width: 300}}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                        cover={<img alt="example" src="https://i.imgur.com/s8C5yGT.jpg" />}
                     >
                         <Meta title="Hà Thanh Huy" description="Software Engineer" />
                         <br />
